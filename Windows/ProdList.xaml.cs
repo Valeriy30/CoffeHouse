15e7d90c86050d056a3f5fd724ca83a63952dc45
xXWispXx <shizogriz@mail.ru>
2023-02-20 16:34:53 +0300

    1

--- Windows/ProdList.xaml.cs
+++ Windows/ProdList.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CoffeeHouse9_14.Pages
+namespace CoffeeHouse9_14.Windows
 {
     /// <summary>
-    /// Логика взаимодействия для ProdListPage.xaml
+    /// Логика взаимодействия для ProdList.xaml
     /// </summary>
-    public partial class ProdListPage : Page
+    public partial class ProdList : Window
     {
-        public ProdListPage()
+        public ProdList()
         {
             InitializeComponent();
         }
