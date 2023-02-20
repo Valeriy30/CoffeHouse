1dca20446d4aeae180a8d03f8add85bf29cbe782
xXWispXx <shizogriz@mail.ru>
2023-02-20 16:47:56 +0300

    1

--- Windows/ProdList.xaml.cs
+++ Windows/ProdList.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using CoffeeHouse9_14.ClassHelper;
+using static CoffeeHouse9_14.ClassHelper.EFClass;
+using CoffeeHouse9_14.Windows;
+using CoffeeHouse9_14.DB;
 
 namespace CoffeeHouse9_14.Windows
 {
@@ -22,6 +27,15 @@ namespace CoffeeHouse9_14.Windows
         public ProdList()
         {
             InitializeComponent();
+            GetProduct();
+        }
+        private void GetProduct()
+        {
+            List<Product> ProdList = new List<Product>();
+
+            ProdList = context.Product.ToList();
+
+            LvProductList.ItemsSource = ProdList;
         }
     }
 }
